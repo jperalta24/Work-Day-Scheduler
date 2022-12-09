96ee12537a3d519b9eb49d766bc64981a38bcf2a
Slender James <114044205+jperalta24@users.noreply.github.com>
2022-12-08 17:26:14 -0800

    set local storage for save button event

--- script.js
+++ script.js
@@ -6,9 +6,20 @@ $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
+  // function? "this" references the button that is being clicked
+  //  How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  // useful when saving the description in local storage?// Line 16. Dom traversal can be
+  // used to move through the dom to target "this" button's parent with with an "id" attr
+  let saveBtn = $(".saveBtn");
+  saveBtn.on('click', function () {
+      let textBlock = $(this).siblings('.description').val(); 
+      let timeBlock = $(this).parent().attr("id");
+      localStorage.setItem(timeBlock, textBlock );
+      console.log(textBlock);
+      console.log(timeBlock);
+    
+    });
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -21,7 +32,7 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-  let currentDay = dayjs().format("MMMM D, YYYY");
+  let currentDay = dayjs().format("ddd, MMMM D, YYYY hh:m A");
 $('#currentDay').text(currentDay);
 });
 
